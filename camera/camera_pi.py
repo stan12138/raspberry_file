083b8ac42360efa3c318480de1fbadde59b82946
stan <ihnyi@qq.com>
2019-11-27 10:25:17 +0800

    2019.11.27

--- camera/camera_pi.py
+++ camera/camera_pi.py
@@ -13,7 +13,7 @@ class Camera(BaseCamera):
 
             stream = io.BytesIO()
             for _ in camera.capture_continuous(stream, 'jpeg',
-                                                 use_video_port=True, resize=(1920, 1080)):
+                                                 use_video_port=True, resize=(1280, 720)):
                 # return current frame
                 stream.seek(0)
                 yield stream.read()
