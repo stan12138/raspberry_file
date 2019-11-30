85d53130a8682b46d2bdf2160fd5619ba97ce8dc
stan <ihnyi@qq.com>
2019-11-30 19:28:50 +0800

    2019.11.30

--- camera/app_test.py
+++ camera/app_test.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python
 from importlib import import_module
 import os
-from flask import Flask, render_template, Response
-import servo
-from camera_pi import Camera
+from flask import Flask, render_template, Response, request, session, escape, redirect, url_for
+# import servo
+# from camera_pi import Camera
 
 app = Flask(__name__)
 
-my_servo = servo.Servo()
+# my_servo = servo.Servo()
 
 @app.route('/')
 def index():
     """Video streaming home page."""
-    return render_template('index.html')
+    # return render_template('index.html')
+    if "user" in session and escape(session["user"])=="stan" :
+        return render_template('index.html')
+    else :
+        session["user"] = "stranger"
+        return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def login() :
+    user = request.form["user"]
+    password = request.form["password"]
+    print(user, password)
+    if user=="Stan" and password=="112358" :
+        session["user"] = "stan"
+        return redirect("/")
+    else :
+        session["user"] = "stranger"
+        return redirect("/")
 
 
 def gen(camera):
@@ -25,38 +43,46 @@ def gen(camera):
 
 @app.route('/video_feed')
 def video_feed():
+    if "user" in session and escape(session["user"])=="stan" :
     """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(Camera()),
+        return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+    else :
+        session["user"] = "stranger"
+        return render_template("login.html")        
 
 
-@app.route("/backend")
-def backend() :
-    return render_template("backend.html")
+# @app.route("/backend")
+# def backend() :
+#     return render_template("backend.html")
 
 
 @app.route("/pitch_add")
 def pitch_add() :
     # print("get pitch add")
-    my_servo.pitch_add(distance=7)
+    if "user" in session and escape(session["user"])=="stan" :
+        my_servo.pitch_add(distance=7)
     return ""
 
 @app.route("/pitch_decrease")
 def pitch_decrease() :
     # print("get pitch decrease")
-    my_servo.pitch_decrease(distance=7)
+    if "user" in session and escape(session["user"])=="stan" :
+        my_servo.pitch_decrease(distance=7)
     return ""
 
 @app.route("/yaw_add")
 def yaw_add() :
     # print("get yaw add")
-    my_servo.yaw_add(distance=7)
+    if "user" in session and escape(session["user"])=="stan" :
+        my_servo.yaw_add(distance=7)
     return ""
 
 @app.route("/yaw_decrease")
 def yaw_decrease() :
     # print("get yaw decrease")
-    my_servo.yaw_decrease(distance=7)
+    if "user" in session and escape(session["user"])=="stan" :
+        my_servo.yaw_decrease(distance=7)
     return ""
 
 
