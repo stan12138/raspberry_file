8f81d9505e00b6b25caa15553d7a4bd5c3a6024b
stan <ihnyi@qq.com>
2019-11-30 18:15:58 +0800

    2019.11.30

--- camera/servo.py
+++ camera/servo.py
@@ -21,22 +21,17 @@ class Servo :
         GPIO.setup(self.pitch_io, GPIO.OUT)
         GPIO.setup(self.yaw_io, GPIO.OUT)
 
-        self.pwm1 = GPIO.PWM(self.pitch_io, 100)
-        self.pwm2 = GPIO.PWM(self.yaw_io, 100)
-        self.pwm1.start(5)
-        self.pwm2.start(5)
-
-        # self.pitch(self.pitch_angle)
-        # self.yaw(self.yaw_angle)
+        self.pitch(self.pitch_angle)
+        self.yaw(self.yaw_angle)
 
 
     def set_angle(self, port, angle) :
-        if port==self.pitch_io :
-            pwm = self.pwm1
-        else :
-            pwm = self.pwm2
-        dutycycle = angle/10 + 2.5
+        pwm = GPIO.PWM(port, 50)
+        pwm.start(8)
+        dutycycle = angle/18 + 3
         pwm.ChangeDutyCycle(dutycycle)
+        sleep(0.3)
+        pwm.stop()
 
     def pitch_add(self, pitch_io=17, distance=5) :
         self.pitch_angle += distance
